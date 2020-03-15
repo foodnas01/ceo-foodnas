7d53447c09867457d61b3465ac05978c7f3a069f
Hassan Raza <hassanuos@users.noreply.github.com>
2020-03-15 11:14:47 +0300

    arabic traslations

---
+++ resources/lang/ar/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+// resources/lang/ar/messages.php
+
+return [
+    'Docs' => 'المستندات',
+    'Laracasts' => 'اللاراكاست',
+    'News' => 'أخبار',
+    'Blog' => 'مدونة',
+    'Nova' => 'نوفا',
+    'Forge' => 'تشكيل',
+    'GitHub' => 'جيثب',
+    'Laravel' => 'Laravel Arabic'
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ __('messages.Laravel') }}
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">{{ __('messages.Docs') }}</a>
+                    <a href="https://laracasts.com">{{ __('messages.Laracasts') }}</a>
+                    <a href="https://laravel-news.com">{{ __('messages.News') }}</a>
+                    <a href="https://blog.laravel.com">{{ __('messages.Blog') }}</a>
+                    <a href="https://nova.laravel.com">{{ __('messages.Nova') }}</a>
+                    <a href="https://forge.laravel.com">{{ __('messages.Forge') }}</a>
+                    <a href="https://github.com/laravel/laravel">{{ __('messages.GitHub') }}</a>
                 </div>
             </div>
         </div>
