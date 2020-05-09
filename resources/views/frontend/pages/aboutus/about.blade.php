779a5a1c27f0a6584f8722f8e2d87ba7a8e11bdc
zohaibyousuf <62107320+zohaibyousuf@users.noreply.github.com>
2020-05-09 18:01:03 +0500

    pushing latest changes

--- resources/views/frontend/pages/aboutus/about.blade.php
+++ resources/views/frontend/pages/aboutus/about.blade.php
@@ -44,51 +44,7 @@
 		</div>
 		<!--/bg_color_1-->
 
-		<div class="container margin_80_55">
-			<div class="main_title_2">
-				<span><em></em></span>
-				<h2>Our founders</h2>
-				<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
-			</div>
-			<div id="carousel" class="owl-carousel owl-theme">
-				<div class="item">
-					<a href="#0">
-						<div class="title">
-							<h4>Julia Holmes<em>CEO</em></h4>
-						</div><img src="img/1_carousel.jpg" alt="">
-					</a>
-				</div>
-				<div class="item">
-					<a href="#0">
-						<div class="title">
-							<h4>Lucas Smith<em>Marketing</em></h4>
-						</div><img src="img/2_carousel.jpg" alt="">
-					</a>
-				</div>
-				<div class="item">
-					<a href="#0">
-						<div class="title">
-							<h4>Paul Stephens<em>Business strategist</em></h4>
-						</div><img src="img/3_carousel.jpg" alt="">
-					</a>
-				</div>
-				<div class="item">
-					<a href="#0">
-						<div class="title">
-							<h4>Pablo Himenez<em>Customer Service</em></h4>
-						</div><img src="img/4_carousel.jpg" alt="">
-					</a>
-				</div>
-				<div class="item">
-					<a href="#0">
-						<div class="title">
-							<h4>Andrew Stuttgart<em>Admissions</em></h4>
-						</div><img src="img/5_carousel.jpg" alt="">
-					</a>
-				</div>
-			</div>
-			<!-- /carousel -->
-		</div>
+		
 		<!--/container-->
 	</main>
 
